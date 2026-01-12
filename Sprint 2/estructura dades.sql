4829951776349d9b3a0c686e70b0f78d1d253c1d
xavigc <60385156+xavigc@users.noreply.github.com>
2026-01-12 12:15:37 +0100

    Add files via upload

---
+++ Sprint 2/estructura dades.sql
@@ -0,0 +1,30 @@
+    -- Creamos la base de datos
+    CREATE DATABASE IF NOT EXISTS transactions;
+    USE transactions;
+
+    -- Creamos la tabla company
+    CREATE TABLE IF NOT EXISTS company (
+        id VARCHAR(15) PRIMARY KEY,
+        company_name VARCHAR(255),
+        phone VARCHAR(15),
+        email VARCHAR(100),
+        country VARCHAR(100),
+        website VARCHAR(255)
+    );
+
+
+    -- Creamos la tabla transaction
+    CREATE TABLE IF NOT EXISTS transaction (
+        id VARCHAR(255) PRIMARY KEY,
+        credit_card_id VARCHAR(15) REFERENCES credit_card(id),
+        company_id VARCHAR(20), 
+        user_id INT REFERENCES user(id),
+        lat FLOAT,
+        longitude FLOAT,
+        timestamp TIMESTAMP,
+        amount DECIMAL(10, 2),
+        declined BOOLEAN,
+        FOREIGN KEY (company_id) REFERENCES company(id) 
+    );
+    
+    
